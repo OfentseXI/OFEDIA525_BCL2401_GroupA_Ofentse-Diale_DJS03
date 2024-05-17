95ded1b2e9f46c09dbe6aa5b497d241ec03830d3
OfentseXI <dofentse69@gmail.com>
2024-05-17 09:43:09 +0200

    syntax adjustments to data search form element

--- scripts.js
+++ scripts.js
@@ -147,28 +147,28 @@ document.querySelector('[data-search-form]').addEventListener('submit', (event) 
         document.querySelector('[data-list-message]').classList.remove('list__message_show')
     }
 
-    document.querySelector('[data-list-items]').innerHTML = ''
-    const newItems = document.createDocumentFragment()
+    document.querySelector('[data-list-items]').innerHTML = '';
+    const newItems = document.createDocumentFragment();
 
-    for (const { author, id, image, title } of result.slice(0, BOOKS_PER_PAGE)) {
-        const element = document.createElement('button')
-        element.classList = 'preview'
-        element.setAttribute('data-preview', id)
-    
-        element.innerHTML = `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[author]}</div>
-            </div>
-        `
-
-        newItems.appendChild(element)
-    }
+    for (const { author, id, image, title } of result.slice(0, BOOKS_PER_PAGE)) 
+            {
+            const element = document.createElement('button')
+            element.classList = 'preview'
+            element.setAttribute('data-preview', id)
+        
+            element.innerHTML = `
+                <img
+                    class="preview__image"
+                    src="${image}"
+                />
+                
+                <div class="preview__info">
+                    <h3 class="preview__title">${title}</h3>
+                    <div class="preview__author">${authors[author]}</div>
+                </div>
+            `;
+            newItems.appendChild(element)
+ }
 
     document.querySelector('[data-list-items]').appendChild(newItems)
     document.querySelector('[data-list-button]').disabled = (matches.length - (page * BOOKS_PER_PAGE)) < 1
