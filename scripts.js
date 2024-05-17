35037378185a35c6c4a70f2eed01f319dfe54571
OfentseXI <dofentse69@gmail.com>
2024-05-17 08:53:38 +0200

    Adjustment of createOptionsFragment function and calling of said function

--- scripts.js
+++ scripts.js
@@ -7,9 +7,9 @@ function renderBookPreviews() {
     const starting = document.createDocumentFragment();
 
     for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-    const element = document.createElement('button')
-    element.classList = 'preview'
-    element.setAttribute('data-preview', id)
+    const element = document.createElement('button');
+    element.classList = 'preview';
+    element.setAttribute('data-preview', id);
 
     element.innerHTML = `
         <img
@@ -23,37 +23,39 @@ function renderBookPreviews() {
         </div>
     `;
     starting.appendChild(element);
-}
+    }
     document.querySelector('[data-list-items]').appendChild(starting)
 }
 renderBookPreviews();
 
-const genreHtml = document.createDocumentFragment()
-const firstGenreElement = document.createElement('option')
-firstGenreElement.value = 'any'
-firstGenreElement.innerText = 'All Genres'
-genreHtml.appendChild(firstGenreElement)
+function createOptionsFragment() { 
+    const genreHtml = document.createDocumentFragment();
+    const firstGenreElement = document.createElement('option');
+    firstGenreElement.value = 'any';
+    firstGenreElement.innerText = 'All Genres';
+    genreHtml.appendChild(firstGenreElement);
 
 for (const [id, name] of Object.entries(genres)) {
-    const element = document.createElement('option')
-    element.value = id
-    element.innerText = name
-    genreHtml.appendChild(element)
+    const element = document.createElement('option');
+    element.value = id;
+    element.innerText = name;
+    genreHtml.appendChild(element);
 }
-
 document.querySelector('[data-search-genres]').appendChild(genreHtml)
+}
+createOptionsFragment();
 
-const authorsHtml = document.createDocumentFragment()
-const firstAuthorElement = document.createElement('option')
-firstAuthorElement.value = 'any'
-firstAuthorElement.innerText = 'All Authors'
-authorsHtml.appendChild(firstAuthorElement)
+const authorsHtml = document.createDocumentFragment();
+const firstAuthorElement = document.createElement('option');
+firstAuthorElement.value = 'any';
+firstAuthorElement.innerText = 'All Authors';
+authorsHtml.appendChild(firstAuthorElement);
 
 for (const [id, name] of Object.entries(authors)) {
-    const element = document.createElement('option')
-    element.value = id
-    element.innerText = name
-    authorsHtml.appendChild(element)
+    const element = document.createElement('option');
+    element.value = id;
+    element.innerText = name;
+    authorsHtml.appendChild(element);
 }
 
 document.querySelector('[data-search-authors]').appendChild(authorsHtml)
